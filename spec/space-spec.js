aaa1a9bd0eb7d32143bd55ae3bb8b3fee47915ff
kelsey guerra <kelseylguerra@gmail.com>
2018-04-20 13:56:42 -0700

    fixed error, test is running, failing

--- spec/space-spec.js
+++ spec/space-spec.js
@@ -2,15 +2,13 @@ import { calculateSeconds } from "./../src/space-class.js";
 import { ageInput } from "./../src/space-class.js";
 
   describe('ageInput', function() {
-  }
 
     it('Should convert years to seconds', function() {
-      expect(calculateSeconds(26)).toEqual(820040000);
+      expect(incalculateSeconds(26)).toEqual(820040000);
     });
-  });
 
-  it('should convert input age from Earth years to Mercury  years', function() {
-    let inputAge = new inputAge(26)
+  it('should convert input age from Earth years to Mercury        years', function() {
+    let inputage = new inputAge(26);
     expect(inputAge.convertMercuryAge()).toEqual(6.24);
   });
 });
