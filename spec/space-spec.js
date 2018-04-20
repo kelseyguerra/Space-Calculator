8bdcaf44b0ede9e8e0a2efb993dbdf2e6920562b
kelsey guerra <kelseylguerra@gmail.com>
2018-04-20 14:21:27 -0700

    wipe specs to get working test functions

--- spec/space-spec.js
+++ spec/space-spec.js
@@ -1,14 +1,11 @@
 import { ageInput } from "./../src/space-class.js";
 
   describe('ageInput', function() {
+  it('Should convert years to seconds', function() {
+    expect(calculateSeconds(26)).toEqual(820040000);
+    })
+  })
 
-    it('Should convert years to seconds', function() {
-      // let age = new ageInput(26);
-      expect(calculateSeconds(26)).toEqual(820040000);
-    });
-//
-//     it('should convert input age from Earth years to Mercury        years', function() {
-//       let age = new ageInput(26);
-//       expect(age.convertMercuryAge()).toEqual(6.24);
-//   });
-// });
+  // it('should convert input age from Earth years to Mercury  years', function() {
+  //   expect(mercuryAge(26)).toEqual(6.266);
+  // })
--- src/space-class.js
+++ src/space-class.js
@@ -1,10 +1,11 @@
 
 class ageInput {
-    constructor (birthDate) {
-    this.birthDate = birthDate;
+
+  constructor (birthDate) {
+  this.birthDate = birthDate;
   }
-  calculateSeconds() {
-    return this.birthDate * 3.154e+7;
+  calculateSeconds(input) {
+    return input * 3.154e+7;
   }
   convertMercuryAge() {
     return this.birthDate*.24;
