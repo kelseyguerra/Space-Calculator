2ac8302ac94838f04d1818108c6a405680ba0417
kelsey guerra <kelseylguerra@gmail.com>
2018-04-20 15:49:59 -0700

    change mercury function to output math.round

--- spec/space-spec.js
+++ spec/space-spec.js
@@ -1,7 +1,7 @@
 import { ageInput } from "./../src/space-class.js";
 
   describe('ageCalculator', function() {
-  
+
   it('will convert users input from years to seconds', function() {
     let ageInput = new Age(26);
     expect(ageInput.calculateSeconds()).toEqual(820040000);
@@ -14,8 +14,3 @@ import { ageInput } from "./../src/space-class.js";
     expect(ageInput.birthDate()).toEqual(820040000);
     })
   })
-
-
-  // it('should convert input age from Earth years to Mercury  years', function() {
-  //   expect(mercuryAge(26)).toEqual(6.266);
-  // })
--- src/space-class.js
+++ src/space-class.js
@@ -1,28 +1,29 @@
-export { ageInput }
+export class ageInput {
+  constructor(age) {
+    this.age = age;
 
-class ageInput {
-  constructor() {
-    this.birthDate;
     // this.presentDate = presentDate;
     // this.mercuryAge = convertMercuryAge();
     // this.venusAge = convertVenusAge();
     // this.marsAge = convertMarsAge();
     // this.jupiterAge = convertJupiterAge();
     // this.ageInSeconds = calculateSeconds();
-}
-calculateSeconds() {
-  return this.birthdate * 3.154e+7;
-}
-convertMercuryAge() {
-  return this.birthDate*.24;
-}
-convertVenusAge() {
-  return this.birthDate*.62;
-}
-convertMarsAge() {
-  return this.birthDate*1.88;
-}
-convertJupiterAge() {
-  return this.birthDate*11.86;
-}
+  }
+  calculateSeconds() {
+    let ageInSeconds = age * 3.154e+7;
+    return ageInSeconds;
+  }
+  convertMercuryAge() {
+    let mercuryAge = age * .24;
+    return Math.round(mercuryAge);
+  }
+  convertVenusAge() {
+    return this.birthDate * .62;
+  }
+  convertMarsAge() {
+    return this.birthDate * 1.88;
+  }
+  convertJupiterAge() {
+    return this.birthDate * 11.86;
+  }
 }
